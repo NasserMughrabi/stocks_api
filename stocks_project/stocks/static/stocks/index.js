61944538cf0476c001f79e6e31725c66f40145d6
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-05-31 13:29:56 -0600

    displays live stock prices.

--- stocks_project/stocks/static/stocks/index.js
+++ stocks_project/stocks/static/stocks/index.js
@@ -1,62 +1,78 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
-    document.querySelector('form').onsubmit = function () {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Host': 'realstonks.p.rapidapi.com',
-                'X-RapidAPI-Key': 'b55775101cmshf7a0fdee78a5340p11b6b4jsnc627709db9a2'
-            }
-        };
-
-        let stock = document.querySelector('#stock').value.toUpperCase();
-        fetch(`https://realstonks.p.rapidapi.com/${stock}`, options)
-            .then(response => response.json())
-            .then(data => {
-                display_stock_data(data);
-            })
-            .catch(err => console.error(err));
-
-
-        // // url for the entered stock by the user
-        // let stock = document.querySelector('#stock').value.toUpperCase();
-        // const stocks_api = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=IZMH0GAQS0RALJ7B`;
-
-
-        // fetch(stocks_api)
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         display_stock_data(data);
-        //     })
-        //     .catch(
-        //         error => {
-        //         console.log('Error:', error)
-        //     });
+    // const defaultInterval = setInterval(fetchStockData, 500);
+    document.querySelector('form').onsubmit = function() {
+        
+        // clearInterval(defaultInterval);
+        // setInterval(fetchStockData, 500);
         return false;
+        
     }
 });
 
+function fetchStockData(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'realstonks.p.rapidapi.com',
+            'X-RapidAPI-Key': 'b55775101cmshf7a0fdee78a5340p11b6b4jsnc627709db9a2'
+        }
+    };
 
-function display_stock_data(stockData){
-    console.log(stockData);
+    let stock = document.querySelector('#stock').value;
+    if(stock){
+        stock = stock.toUpperCase();
+    }else {
+        stock = 'AMD';
+    }
 
-    const price = stockData.price;
-    console.log(price);
+
+    fetch(`https://realstonks.p.rapidapi.com/${stock}`, options)
+        .then(response => response.json())
+        .then(data => {
+            // call this function every two seconds
+            display_stock_data(data, stock);
+        })
+        .catch(err => console.error(err));
+
+    return false;
+}
+
+
+function display_stock_data(stockData, stockName){
+    
+    const price = stockData.price.toFixed(2);
     const change_percentage = stockData.change_percentage;
-    console.log(change_percentage);
     const change_point = stockData.change_point;
-    console.log(change_point);
     const total_vol = stockData.total_vol;
-    console.log(total_vol);
 
-    const dataDiv = document.createElement('div');
-    dataDiv.innerHTMl = `
-    <div>Current Price: ${price}</div>
-    <div>Change Percentage: ${change_percentage}%</div>
-    <div>Change Point: ${change_point}</div>
-    <div>Total Volume: ${total_vol}</div>
-    `;
+    const current_stock_color = decide_color(change_point);
+    // if the color is green, add plus to the start of the percentage and point numbers
+    let plus = '';
+    if(current_stock_color === '#08F6B5'){ // if color is green
+        plus = '+';
+    }
 
-    document.querySelector('#result').innerHTML = dataDiv.innerHTMl ;
+    document.querySelector('#stock-ticker').innerHTML = stockName;
+    document.querySelector('#price-div').innerHTML = price;
+    document.querySelector('#perc-div').innerHTML = plus + change_percentage + '%';
+    document.querySelector('#points-div').innerHTML = plus + change_point;
+    document.querySelector('#stock-fullname').innerHTML = stockName;
+    document.querySelector('#stock-ETF').innerHTML = 'NASDAQ';
+    document.querySelector('#stock-name').innerHTML = (stockName);
+    
+    document.querySelector('#price-div').style.color = current_stock_color;
+    document.querySelector('#perc-div').style.color = current_stock_color;
+    document.querySelector('#points-div').style.color = current_stock_color;
+}
+
+
+function decide_color(dataAttribute){
+    if(dataAttribute > 0){
+        return '#08F6B5';
+    } else if (dataAttribute === 0){
+        return 'white';
+    }else {
+        return '#FF506A'
+    }
 }
