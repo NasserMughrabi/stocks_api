e5cd4e71a78d92d7cb3545b50d0bab8c9ec0b6aa
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-06-03 12:43:23 -0600

    more styling added

--- stocks_project/stocks/static/stocks/index.js
+++ stocks_project/stocks/static/stocks/index.js
@@ -1,16 +1,41 @@
+var submit = null;
+
 document.addEventListener('DOMContentLoaded', function () {
 
+    fetchStockData('AAPL');
+    fetchPopStocks();
+    document.querySelector('#AAPL').addEventListener('click', ()=> fetchStockData('AAPL'));
+    document.querySelector('#TSLA').addEventListener('click', ()=>  fetchStockData('TSLA'));
+    document.querySelector('#AMZN').addEventListener('click',  ()=> fetchStockData('AMZN'));
+    document.querySelector('#AMD').addEventListener('click', ()=> fetchStockData('AMD'));
+    document.querySelector('#NVDA').addEventListener('click', ()=> fetchStockData('NVDA'));
+
+    // setInterval(fetchPopStocks, 500);
     // const defaultInterval = setInterval(fetchStockData, 500);
     document.querySelector('form').onsubmit = function() {
         
         // clearInterval(defaultInterval);
-        setInterval(fetchStockData, 500);
+        // if(submit){
+        //     console.log('here');
+        //     clearInterval(submit);
+        // }
+        // setTimeout(function(){}, 500);
+        let stock = document.querySelector('#stock').value;
+        if(stock){
+            stock = stock.toUpperCase();
+        }else {
+            stock = 'AAPL';
+        }
+        // while(true){
+        fetchStockData(stock);
+        // }
+        // submit = setInterval(function(){fetchStockData(stock);}, 500);
+        // clearInterval(submit);
         return false;
-        
     }
 });
 
-function fetchStockData(){
+function fetchStockData(stock){
     const options = {
         method: 'GET',
         headers: {
@@ -19,13 +44,6 @@ function fetchStockData(){
         }
     };
 
-    let stock = document.querySelector('#stock').value;
-    if(stock){
-        stock = stock.toUpperCase();
-    }else {
-        stock = 'AMD';
-    }
-
 
     fetch(`https://realstonks.p.rapidapi.com/${stock}`, options)
         .then(response => response.json())
@@ -33,7 +51,7 @@ function fetchStockData(){
             // call this function every two seconds
             display_stock_data(data, stock);
         })
-        .catch(err => console.error(err));
+        .catch(err => alert('Invalid stock ticker'));
 
     return false;
 }
@@ -49,27 +67,67 @@ function display_stock_data(stockData, stockName){
     const current_stock_color = decide_color(change_point);
     // if the color is green, add plus to the start of the percentage and point numbers
     let plus = '';
-    if(current_stock_color === '#08F6B5'){ // if color is green
+    if(current_stock_color === '#11DF44'){ // if color is green
         plus = '+';
     }
 
-    document.querySelector('#stock-ticker').innerHTML = stockName;
-    document.querySelector('#price-div').innerHTML = price;
-    document.querySelector('#perc-div').innerHTML = plus + change_percentage + '%';
-    document.querySelector('#points-div').innerHTML = plus + change_point;
-    document.querySelector('#stock-fullname').innerHTML = stockName;
-    document.querySelector('#stock-ETF').innerHTML = 'NASDAQ';
-    document.querySelector('#stock-name').innerHTML = (stockName);
+    document.querySelector('#name-ticker').innerHTML = stockName;
+    document.querySelector('#price').innerHTML = price;
+    document.querySelector('#percentage').innerHTML = plus + change_percentage + '%';
+    document.querySelector('#points').innerHTML = plus + change_point;
+    document.querySelector('#stock-name-div').innerHTML = stockName;
     
-    document.querySelector('#price-div').style.color = current_stock_color;
-    document.querySelector('#perc-div').style.color = current_stock_color;
-    document.querySelector('#points-div').style.color = current_stock_color;
+    document.querySelector('#price').style.color = current_stock_color;
+    document.querySelector('#percentage').style.color = current_stock_color;
+    document.querySelector('#points').style.color = current_stock_color;
 }
 
+function fetchPopStocks(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'realstonks.p.rapidapi.com',
+            'X-RapidAPI-Key': 'b55775101cmshf7a0fdee78a5340p11b6b4jsnc627709db9a2'
+        }
+    };
+
+    var popStocks = new Array('AAPL', 'TSLA', 'AMZN', 'AMD', 'NVDA');
+    popStocks.forEach( popStock => {
+        fetch(`https://realstonks.p.rapidapi.com/${popStock}`, options)
+        .then(response => response.json())
+        .then(data => {
+            // call this function every two seconds
+            display_pop_stocks(data, popStock);
+        })
+        .catch(err => alert('Invalid stock ticker'));//console.error(err)
+
+        return false;
+    });
+    
+}
+
+function display_pop_stocks(stockData, stockName){
+
+    const price = stockData.price.toFixed(2);
+    const change_percentage = stockData.change_percentage;
+    const change_point = stockData.change_point;
+    const total_vol = stockData.total_vol;
+
+    const current_stock_color = decide_color(change_point);
+    // if the color is green, add plus to the start of the percentage and point numbers
+    let plus = '';
+    if(current_stock_color === '#11DF44'){ // if color is green
+        plus = '+';
+    }
+
+    document.querySelector(`#${stockName}-price`).innerHTML = price;
+    document.querySelector(`#${stockName}-perc`).innerHTML = plus + change_percentage + '%';
+    document.querySelector(`#${stockName}-points`).innerHTML = plus + change_point;
+}
 
 function decide_color(dataAttribute){
     if(dataAttribute > 0){
-        return '#08F6B5';
+        return '#11DF44';
     } else if (dataAttribute === 0){
         return 'white';
     }else {
